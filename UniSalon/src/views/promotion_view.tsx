d998ff6ebe1c5096ca7dc61525330bd365082936
SiPav <longthavsipav69@gmail.com>
2023-10-27 11:31:54 +0700

    Update

--- UniSalon/src/views/promotion_view.tsx
+++ UniSalon/src/views/promotion_view.tsx
@@ -1,48 +1,67 @@
 import React from "react";
-import { SafeAreaView, Text, View, useWindowDimensions, Image } from "react-native";
+import { SafeAreaView, Text, View, useWindowDimensions, Image, Dimensions, TouchableOpacity } from "react-native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet } from "react-native";
-import { TabView, SceneMap } from 'react-native-tab-view';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 import Feather from 'react-native-vector-icons/Feather';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import { Navigation } from 'react-native-navigation';
 
+const { width } = Dimensions.get('window')
 const Stack = createNativeStackNavigator();
 
-export const Promotion = () => (
-    <View style={styles.CardPromotion}>
-        <View style={{
-            width: 90,
-            height: 90,
-            backgroundColor: '#ebebec',
-            borderRadius: 10,
+const Promotion = () => (
+    <View style={{ width: 'auto', height: '94%', backgroundColor: '#f1f1f1' }}>
+        <TouchableOpacity style={{ width: 'auto', height: '94%' }} 
+        onPress={() => {
+            console.log("Start press!");
         }}>
-            {/* <Image source={require('assets/imgs/img1.jpg')}></Image> */}
-        </View>
-        <View style={{ paddingLeft: 10, alignItems: 'flex-start' }}>
-            <Text style={{
-                fontSize: 18,
-                color: 'black',
-                // marginTop: 10
-            }}>កាត់សក់បុរស</Text>
-            <Text>មែន​ ស្តាយ</Text>
-            <Text>⭐⭐⭐⭐⭐ (3)</Text>
-            <Text>កាត់សក់បុរស free កក់សក់ជូន</Text>
-
-        </View>
-    </View>
-);
-
-const Height = () => (
-    <View style={{ flex: 1, justifyContent: "center", alignContent: 'center', alignItems: 'center' }}>
-        <Text>Height</Text>
+            <View style={styles.CardPromotion}>
+                <View style={{
+                    width: '40%',
+                    height: '100%',
+                    backgroundColor: '#ebebec',
+                    borderRadius: 10,
+                }}>
+                    <Image style={{
+                        width: undefined, height: undefined, flex: 1, borderRadius: 8
+                    }} source={require('../../assets/imgs/img1.jpg')}></Image>
+                </View>
+                <View style={{ paddingLeft: 10, alignItems: 'flex-start' }}>
+                    <Text style={{
+                        fontSize: 18,
+                        color: 'black',
+                        // marginTop: 10
+                    }}>កាត់សក់បុរស</Text>
+                    <Text style={{
+                        color: '#16247d',
+                        fontSize: 15,
+                        fontWeight: 'bold'
+                    }}>មែន​ ស្តាយ</Text>
+                    <Text>⭐⭐⭐⭐⭐ (3)</Text>
+                    <Text>កាត់សក់បុរស free កក់សក់ជូន</Text>
+                    <View style={{ flexDirection: 'row', }}>
+                        <View style={{ flexDirection: 'row', paddingVertical: 15, paddingRight: 20 }}>
+                            <FontAwesome6 name="location-dot" style={{ paddingRight: 5, marginTop: 4 }} color={'#16247d'}></FontAwesome6>
+                            <Text style={{ color: '#16247d', fontWeight: 'bold' }}>None</Text>
+                        </View>
+                        <View style={{ flexDirection: 'row', paddingVertical: 15 }}>
+                            <FontAwesome6 name="clock" style={{ paddingRight: 5, marginTop: 4 }} color={'#16247d'}></FontAwesome6>
+                            <Text style={{ color: '#16247d', fontWeight: 'bold' }}>Opening</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </TouchableOpacity>
     </View>
 );
 
 const Reward = () => (
-    <View style={{ flex: 1, justifyContent: "center", alignContent: 'center', alignItems: 'center' }}>
-        <Text>Reward</Text>
+    <View style={{ backgroundColor: '#f1f1f1', flex: 1, justifyContent: 'flex-start', alignContent: 'center', alignItems: 'center', paddingTop: '10%' }}>
+        <Feather name="folder" size={60} color={'#ebebec'}></Feather>
+        <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#ebebec', paddingTop: '2%' }}>No Data</Text>
     </View>
 );
 
@@ -60,6 +79,30 @@ export const PromotionView = () => {
         // { key: 'second', title: '|' },
         { key: 'third', title: 'Rewards' },
     ]);
+    const renderTabBar = (props: any) => (
+        <TabBar
+            {...props}
+            scrollEnabled
+            indicatorStyle={styles.indicator}
+            style={{
+                backgroundColor: '#f2f2f2'
+            }}
+            tabStyle={{
+                width: width / 2,
+                // backgroundColor: 'blue'
+            }}
+            // renderLabel={({props, }) => (
+            //     <Text style={{ color: 'grey', margin: 8 }}>
+            //       {props.route.title}
+            //     </Text>
+            //   )}
+            renderLabel={({ route, focused, color }) => (
+                <Text style={{ color: focused ? '#16247d' : 'black', margin: 8, fontSize: 18, fontWeight: 'bold' }}>
+                    {route.title}
+                </Text>
+            )}
+        ></TabBar>
+    );
     return (
         <SafeAreaView>
             <View style={styles.Appbar}>
@@ -88,30 +131,21 @@ export const PromotionView = () => {
                     }}>0 Point</Text>
                 </View>
             </View>
-            <View style={{ width: 'auto', height: 5, backgroundColor: '#ebebec', marginVertical: 15 }}>
+            <View style={{ width: 'auto', height: 5, backgroundColor: '#ebebec', marginTop: '8%' }}>
             </View>
-
-            <View style={styles.BottomTab}>
-                <Text style={{
-                    fontSize: 18,
-                }}>Promotions</Text>
-                <View style={{ height: 20, backgroundColor: 'grey', width: 1, marginHorizontal: 50 }}></View>
-                <Text style={{ fontSize: 18 }}>Rewards</Text>
-            </View>
-            <View style={{ width: 'auto', backgroundColor: '#ebebec', height: 1 }}></View>
-            <View >
-            </View>
-            {/* <View style={styles.TabBar}>
+            <View style={styles.TabBar}>
                 <TabView
                     navigationState={{ index, routes }}
                     renderScene={renderScene}
                     onIndexChange={setIndex}
                     initialLayout={{ width: layout.width }}
                     style={{
-                        backgroundColor: 'white',
+                        backgroundColor: 'white'
                     }}
+                    renderTabBar={renderTabBar}
+
                 />
-            </View> */}
+            </View>
             <View style={styles.CardPromotion}>
                 <View style={{
                     width: 90,
@@ -170,18 +204,28 @@ const styles = StyleSheet.create({
 
     TabBar: {
         width: 'auto',
-        height: 800,
+        height: '80%',
+        // backgroundColor: 'red'
     },
     CardPromotion: {
         width: 'auto',
-        height: 100,
-        borderWidth: 3,
+        height: '20%',
+        borderWidth: 0.5,
         borderColor: '#ebebec',
         margin: 15,
+        backgroundColor: '#f1f1f1',
         borderRadius: 10,
         // justifyContent: 'center',
         alignItems: 'center',
-        paddingHorizontal: 8,
-        flexDirection: 'row'
+        flexDirection: 'row',
+        paddingHorizontal: 10,
+        paddingVertical: 10
+    },
+    TabBarView: {
+
+    },
+    indicator: {
+        backgroundColor: '#16247d',
+        height: 2
     }
 });
